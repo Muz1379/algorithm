06cb550e8c464cd65c8c595f9871fda7b68704db
学生李令新 <stullx@qq.com>
2018-03-15 11:14:56 +0800

    快速排序，分治法，先选取一个标记值进行比较，分别将大于标记值的移动到标记值的左右两侧，再对两侧分别进行递归排序。

---
+++ src/main/java/QuickSort.java
@@ -0,0 +1,51 @@
+/**
+ * @author: 李令新 stullx@qq.com
+ * @date: 2018.3.12
+ * @decription: 快速排序
+ */
+public class QuickSort
+    {
+        /**
+         * 快速排序算法
+         *
+         * @param array
+         *         待排序数组
+         * @param start
+         *         起始位置
+         * @param end
+         *         结束位置
+         *
+         * @return
+         */
+        int[] quickSort(int[] array, int start, int end)
+            {
+                if (start >= end)
+                {
+                    return array;
+                }
+                int i = start;
+                int j = end;
+                
+                int key = array[start];
+                /**
+                 * 下面while循环分割待排序数组
+                 */
+                while (i < j)
+                {
+                    while (array[j] >= key && i < j)
+                    {
+                        j--;
+                    }
+                    array[i]=array[j];
+                    while (array[i] <= key && i < j)
+                    {
+                        i++;
+                    }
+                    array[j]=array[i];
+                }
+                array[i]=key;
+                quickSort(array, start, i-1);
+                quickSort(array, i+1, end);
+                return array;
+            }
+    }
